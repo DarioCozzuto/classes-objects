ef6b270a0b7a8a94075c86d77b14ef719a28d4ad
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-23 22:26:49 +0100

    Add files via upload

---
+++ classes objects.py
@@ -0,0 +1,112 @@
+#Create a class named MyClass, with a property named x:
+class MyClass:
+  x = 5
+
+print(MyClass)
+
+
+
+#Create an object named p1, and print the value of x:
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+
+
+#Insert a function that prints a greeting, and execute it on the p1 object:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc() 
+
+
+
+
+#Use the words mysillyobject and abc instead of self:
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc() 
+
+
+
+
+
+#Set the age of p1 to 40:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+p1.age = 40
+
+print(p1.age)
+
+
+
+
+
+#Delete the age property from the p1 object:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+del p1.age
+
+print(p1.age)
+
+
+
+
+
+#Delete the p1 object:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+
+del p1
+
+print(p1)
+
+
+
+
+#having an empty class definition like this, would raise an error without the pass statement
+class Person:
+  pass
+
+
+
+
+
